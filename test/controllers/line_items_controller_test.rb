c0cbd209d64b11b30d795fe5d7c8b29af240c644
roman-developer <jose.maria.roman87@gmail.com>
2020-07-12 12:23:20 +0200

    check out

--- test/controllers/line_items_controller_test.rb
+++ test/controllers/line_items_controller_test.rb
@@ -15,16 +15,6 @@ class LineItemsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should create line_item" do
-    assert_difference('LineItem.count') do
-      post line_items_url, params: { product_id: products(:ruby).id }
-    end
-
-    follow_redirect!
-    assert_select 'h2', 'Your Cart'
-    assert_select 'td', "Programming Ruby 1.9"
-  end
-
   test "should create line_item via ajax" do
     assert_difference('LineItem.count') do
       post line_items_url, params: { product_id: products(:ruby).id }, xhr: true
