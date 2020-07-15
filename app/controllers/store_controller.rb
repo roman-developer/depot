099b521979847a1ff2373d26f93346a3affccf22
roman-developer <jose.maria.roman87@gmail.com>
2020-07-15 17:48:39 +0200

    Loggin in

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,6 +1,9 @@
 class StoreController < ApplicationController
+  skip_before_action :authorize
+
   include CurrentCart
   before_action :set_cart
+  
   def index
     @products = Product.order(:title)
   end
